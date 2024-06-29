95072a31b97484a7bc5be9a46bf53ece267a9345
Maria Leitão <malufln.99@gmail.com>
2024-06-29 18:43:28 -0300

    Enhanced CLI with commands for searching IPs and server names using net package

--- app/app.go
+++ app/app.go
@@ -12,17 +12,24 @@ func Generate() *cli.App {
 	app.Name = "CLI Application"
 	app.Usage = "Searches IPs and Sever Names on the Internet"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "google.com",
+		}}
+
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Search IP Adresses on the web",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "google.com",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Searches IP Adresses on the web",
+			Flags:  flags,
 			Action: searchIPs,
+		},
+		{
+			Name:   "servers",
+			Usage:  "Searches Server Names on the web",
+			Flags:  flags,
+			Action: searchServers,
 		},
 	}
 
@@ -39,5 +46,18 @@ func searchIPs(c *cli.Context) {
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+
+func searchServers(c *cli.Context) {
+	host := c.String("host")
+
+	servers, err := net.LookupNS(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, servers := range servers {
+		fmt.Println(servers.Host)
 	}
 }
--- main.go
+++ main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"github.com/marialuizaleitao/website-info-cli/app"
 	"log"
 	"os"
 )
 
 func main() {
-	fmt.Println("Starting point")
-
 	application := app.Generate()
 	if err := application.Run(os.Args); err != nil {
 		log.Fatal(err)

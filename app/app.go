6cba2fbfe6d988d46206e3edbca9b4a2af54ef98
Maria Leitão <malufln.99@gmail.com>
2024-06-29 18:32:15 -0300

    Added CLI command for searching IPs using net package

--- app/app.go
+++ app/app.go
@@ -1,11 +1,43 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"github.com/urfave/cli"
+	"log"
+	"net"
+)
 
 func Generate() *cli.App {
 	app := cli.NewApp()
 	app.Name = "CLI Application"
 	app.Usage = "Searches IPs and Sever Names on the Internet"
 
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Search IP Adresses on the web",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "google.com",
+				},
+			},
+			Action: searchIPs,
+		},
+	}
+
 	return app
+}
+
+func searchIPs(c *cli.Context) {
+	host := c.String("host")
+
+	ips, err := net.LookupIP(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
